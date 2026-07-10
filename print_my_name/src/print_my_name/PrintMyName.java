3630ede932567a80171087e9baba7c30a169bf07
cj_dev <cj2349@naver.com>
2026-07-10 14:56:17 +0900

    첫번째 애플리케이션 실습

---
+++ print_my_name/src/print_my_name/PrintMyName.java
@@ -0,0 +1,7 @@
+package print_my_name;
+
+public class PrintMyName {
+	public static void main(String[] args) {
+		System.out.println("내 이름은 임창진 입니다.");
+	}
+}
---
+++ print_my_name_age/src/print_my_name_age/PrintMyInfo.java
@@ -0,0 +1,10 @@
+package print_my_name_age;
+
+public class PrintMyInfo {
+	public static void main(String[] args) {
+		System.out.println("내 이름은 " + '"'+ "임창진" + '"'+ " 이고, 나이는 " + '"'+"27" +'"'+ "세 입니다.");
+		
+		System.out.println("내 이름은 \"임창진\" 이고, 나이는 \"27\"세 입니다.");
+	}
+
+}
