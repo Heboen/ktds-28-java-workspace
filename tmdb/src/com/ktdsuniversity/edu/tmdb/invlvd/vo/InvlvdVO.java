6ef9ad087262ec9187818ea05ad96f7513bf46bd
changjin <cj2349@naver.com>
2026-08-28 17:45:57 +0900

    0828 tmdb invlvdVO

--- tmdb/src/com/ktdsuniversity/edu/tmdb/invlvd/vo/InvlvdVO.java
+++ tmdb/src/com/ktdsuniversity/edu/tmdb/invlvd/vo/InvlvdVO.java
@@ -2,4 +2,33 @@ package com.ktdsuniversity.edu.tmdb.invlvd.vo;
 
 public class InvlvdVO {
 
+	private String invlvdId;
+	private String invlvdPrfl;
+	private String invlvdNm;
+	public String getInvlvdId() {
+		return this.invlvdId;
+	}
+	public void setInvlvdId(String invlvdId) {
+		this.invlvdId = invlvdId;
+	}
+	public String getInvlvdPrfl() {
+		return this.invlvdPrfl;
+	}
+	public void setInvlvdPrfl(String invlvdPrfl) {
+		this.invlvdPrfl = invlvdPrfl;
+	}
+	public String getInvlvdNm() {
+		return this.invlvdNm;
+	}
+	public void setInvlvdNm(String invlvdNm) {
+		this.invlvdNm = invlvdNm;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
+	
 }
